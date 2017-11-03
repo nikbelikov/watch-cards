7752aae628b6d4f3ee3f5182f58255e8b6c8fb6e
Nikolay Belikov <nikmail88@gmail.com>
2017-11-03 22:55:35 +0400

    favicon

--- website/src/html.js
+++ website/src/html.js
@@ -1,5 +1,9 @@
 import React from "react";
-import favicon from './favicon.png';
+import favicon16x16 from './images/favicons/favicon-16x16.png';
+import favicon32x32 from './images/favicons/favicon-32x32.png';
+import appleTouchIcon from './images/favicons/apple-touch-icon.png';
+import manifest from './images/favicons/manifest.json';
+import safariTabIcon from './images/favicons/safari-pinned-tab.svg';
 
 let stylesStr
 if (process.env.NODE_ENV === `production`) {
@@ -30,7 +34,12 @@ module.exports = class HTML extends React.Component {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <link rel="icon" type="image/png" sizes="32x32" href={favicon} />
+        <link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon}/>
+        <link rel="icon" type="image/png" sizes="32x32" href={favicon32x32}/>
+        <link rel="icon" type="image/png" sizes="16x16" href={favicon16x16}/>
+        <link rel="manifest" href={manifest}/>
+        <link rel="mask-icon" href={safariTabIcon} color="#5bbad5"/>
+        <meta name="theme-color" content="#ffffff"/>
         <title>Watchcards.ru - Карточки для Apple Watch</title>
         {this.props.headComponents}
         {css}
